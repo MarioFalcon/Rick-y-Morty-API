d0d692e6b6ff5d2a240a2066d7977f016d0e9bdd
Agustin <agustinescalanterojo@gmail.com>
2023-06-10 20:06:40 +0200

    feat: create Login

---
+++ src/views/Auth/Login/index.tsx
@@ -0,0 +1,22 @@
+import { FC, memo } from "react"
+
+const Login: FC = () => {
+    const { handleonsubmit } = useLogic()
+
+    return (
+
+
+
+
+
+
+
+
+
+
+
+    )
+}
+
+export default memo(Login)
+
