0a213a26ba52f10357db6e2f7a501cbdf8334e9d
Agustin <agustinescalanterojo@gmail.com>
2023-06-11 02:05:18 +0200

    feat: implement firebase

--- src/services/firebase/app.ts
+++ src/services/firebase/app.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    // storageBucket: "nasa-in-react.appspot.com",
-    // messagingSenderId: "1033280009503",
-    // appId: "1:1033280009503:web:96500ef252acb375b4ac2a",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: "rick-y-morty-9f7a5.appspot.com",
+    messagingSenderId: "25847775457",
+    appId: "1:25847775457:web:4f108e44bd86202c4d77a0"
   };
 
 const app = initializeApp(firebaseConfig)
