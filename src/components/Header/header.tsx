c51ea2419144b3a04e65c83e2c8a9b6b1fb1787c
Agustin <agustinescalanterojo@gmail.com>
2023-06-12 21:18:38 +0200

    feat: implement logout

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react';
 import { Container, ButtonContainer, Button } from './headerStyles'
+import useLogic from './headerLogic';
 
 const Header: FC = () => {
+const { handleLogout } = useLogic()
+
   return (
     <Container>
       <ButtonContainer>
         <Button>Reset</Button>
         <Button>Profile</Button>
-        <Button>Logout</Button>
+        <Button onClick={handleLogout}>Logout</Button>
       </ButtonContainer>
     </Container>
   );
