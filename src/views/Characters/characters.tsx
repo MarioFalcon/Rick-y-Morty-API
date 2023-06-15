162db5b915c71faa4d851ec6f576b4a4082af89b
MarioFalcon <maok.seven@gmail.com>
2023-06-16 01:33:04 +0200

    feat: Add Button Back

--- src/views/Characters/characters.tsx
+++ src/views/Characters/characters.tsx
@@ -1,18 +1,26 @@
-import { FC, memo } from 'react'
+import { FC, memo, useCallback } from 'react'
 
 import Header from '../../components/Header/header'
 import Footer from '../../components/Footer/footer'
 import Card from '../../components/Card/card'
 import VideoBackground from '../../components/VideoBackground/videoBackground'
-import BackArrow from '../../components/Back/backArrow'
 
-import { Container, DashboardCards } from './charactersStyles'
+
+import { Container, DashboardCards, Back } from './charactersStyles'
+import { useNavigate } from 'react-router-dom'
 
 
 const Characters: FC = () => {
+  
+  const navigate = useNavigate()
+  const handleGoToBack = useCallback(() => {
+    navigate('/selection')
+  }, [navigate])
+
+
   return (
     <Container>
-      <BackArrow />
+<Back onClick={handleGoToBack}>Back</Back>
     <DashboardCards>
     <Header onLogout={function (): void {}} />
       <VideoBackground videoSrc="/realism.mp4" />
--- src/views/Episodes/episodes.tsx
+++ src/views/Episodes/episodes.tsx
@@ -1,22 +1,30 @@
-import {FC, memo} from 'react'
+import {FC, memo, useCallback} from 'react'
 
 import Header from '../../components/Header/header'
 import Footer from '../../components/Footer/footer'
 import ContainerList from '../../components/ContainerList/containerlist'
-import BackArrow from '../../components/Back/backArrow'
+
 import VideoBackground from '../../components/VideoBackground/videoBackground'
 import {
+  Back,
   DashboardContainer,
   DashboardContent,
   DashboardCards,
 } from './episodesStyles'
-
+import { useNavigate } from 'react-router-dom'
 
 const Episodes: FC = () => {
 
+
+  const navigate = useNavigate()
+  const handleGoToBack = useCallback(() => {
+    navigate('/selection')
+  }, [navigate])
+
+
   return (
     <DashboardContainer>
-         <BackArrow />
+      <Back onClick={handleGoToBack}>Back</Back>
       <Header onLogout={function (): void {}} />
       <VideoBackground videoSrc="/realism.mp4" />
       <DashboardContent>
